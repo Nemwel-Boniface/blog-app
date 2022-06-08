9dd9d28f08f63bffc9cd23facc5329f8e1cc15f0
unknown <okorojiebube2@gmail.com>
2022-06-08 03:17:12 -0600

    Setup controllers

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,14 @@
+class UsersController < ApplicationController
+  def index
+  end
+
+  def show
+    
+  end
+
+  def method_name
+    
+  end
+  
+  
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  get 'posts/index'
+  get 'users/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # Defines the root path route ("/")
-  root "userss#index"
+  root "users#index"
   resources :users do
     resources :posts
   end
