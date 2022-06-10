4017401fb8caa64bca005bf088d179fa2a68a91e
unknown <okorojiebube2@gmail.com>
2022-06-10 06:54:34 -0600

    Add test to return 200 status code for users

---
+++ spec/requests/posts_spec.rb
@@ -0,0 +1,40 @@
+require 'rails_helper'
+
+# describe "Posts", type: :request do
+
+#   before(:each) { get posts_path } 
+
+#   context "index" do
+#     it "GET /index returns okay status" do
+#       expect(response).to have_http_status(200)  
+#     end
+
+#     it 'GET /index renders correct template(index)' do
+#       expect(response).to render_template(:index)
+#     end
+
+#     it 'GET /index include correct placeholder text' do
+#       expect(response.body).to include('See Blog posts')
+#     end
+#   end
+# end
+
+# describe "Posts", type: :request do
+
+#   before(:each) { get post_path({id: 1}) } 
+#   context "show" do
+#     it 'GET /show status' do
+#     expect(response).to have_http_status(200)
+#   end
+
+#   it 'GET /show render correct template(show)' do
+#     expect(response).to render_template(:show)
+#   end
+
+#   it 'GET /show include correct placeholder text' do
+#     expect(response.body).to include('Here is a list of posts for a given user')
+#   end
+#   end
+# end
+
+
