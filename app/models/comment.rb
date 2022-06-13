e32a3d41755638942117de8656258568636cba35
unknown <okorojiebube2@gmail.com>
2022-06-13 05:11:32 -0600

    Add current user

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,10 +1,11 @@
 class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :post
+  after_save :update_comments_counter
 
   private
 
   def update_comments_counter
-    user.increment(:comments_counter)
+    post.update(comments_counter: post.comments.size)
   end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -2,11 +2,12 @@ class Post < ApplicationRecord
   belongs_to :user
   has_many :likes
   has_many :comments
+  after_save :update_posts_counter
 
   private
 
   def update_posts_counter
-    user.increment(:posts_counter)
+    user.update(posts_counter: user.posts.size)
   end
 
   def recent_comments
