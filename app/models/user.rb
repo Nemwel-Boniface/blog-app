1e6f4f0f17c6c5401c8a8723b5daa97d2af72e59
unknown <okorojiebube2@gmail.com>
2022-06-13 04:10:39 -0600

    Add recent post methdd

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,12 @@ class User < ApplicationRecord
   has_many :comments
   has_many :likes
   has_many :posts
+
+  private
+
+  def recent_posts
+    Post.order(created_at: :desc).limit(3)
+  end
 end
+
+
