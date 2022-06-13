14969cafac76d7856c20793ffd44fbdbb08ca8af
unknown <okorojiebube2@gmail.com>
2022-06-13 04:30:05 -0600

    Add recent comments method

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,7 +3,14 @@ class Post < ApplicationRecord
   has_many :likes
   has_many :comments
 
+  private
+
   def update_post_counter
     user.increment(:posts_counter)
   end
+
+  def recent_comments
+    comments.order(created_at: :desc).limit(5)
+  end
+  
 end
