1c57a47984ec9ecf49ac7d3e1dea1f571b65f680
unknown <okorojiebube2@gmail.com>
2022-06-15 15:33:54 -0600

    Modify views

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_10_203834) do
+ActiveRecord::Schema[7.0].define(version: 2022_06_15_200653) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -36,11 +36,11 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_10_203834) do
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "text"
-    t.integer "comments_counter"
     t.integer "likes_counter"
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "comments_counter"
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
