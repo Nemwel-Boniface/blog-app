b1137330d51a3be3643bfe754c1e68c9a5f79588
unknown <okorojiebube2@gmail.com>
2022-06-17 00:08:46 -0600

    Fix linter errors

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,16 +2,16 @@ class CommentsController < ApplicationController
   def new
     @comment = Comment.new
   end
- 
+
   def create
     @comment = Comment.new(comment_params)
     new_comment.author_id = user.id
     new_comment.post_id = post.id
     if @comment.save
-    flash[:success] = "New comment saved successfully"
+      flash[:success] = 'New comment saved successfully'
       redirect_to @comment
     else
-     flash.now[:error] = "Error: Comment could not be saved"
+      flash.now[:error] = 'Error: Comment could not be saved'
       render :new, status: :unprocessable_entity
     end
   end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,34 +2,32 @@ class PostsController < ApplicationController
   def index
     user_id = params[:user_id]
     @user = User.find(user_id)
-    @posts = Post.includes(:user).where(user: params[:user_id]).references(:user)
+    @posts = @user.posts
   end
 
   def show
     @post = Post.includes(:user, comments: [:user]).find(params[:id])
   end
 
-  def new 
+  def new
     @post = Post.new
   end
-  
+
   def create
     @post = Post.new(post_params)
     @post.comments_counter = 0
     @post.likes_counter = 0
-    
+
     if @post.save
-      flash[:success] = "Post saved successfully"
+      flash[:success] = 'Post saved successfully'
       redirect_to @post
     else
-      flash.now[:error] = "Error: Post could not be saved"
+      flash.now[:error] = 'Error: Post could not be saved'
       render :new, status: :unprocessable_entity
     end
   end
-  
+
   def post_params
     params.require(:post).permit(:title, :text)
   end
-  
-
 end
