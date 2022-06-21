8a9e57ff5618304f0bc9074c60bbd25472c9f436
unknown <okorojiebube2@gmail.com>
2022-06-21 08:39:40 -0600

    Add confirmable

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
   # before_action :authenticate_user!
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
+  #  :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable, :confirmable,
          :recoverable, :rememberable, :validatable
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
+  root "users#index"
   devise_for :users, controllers: { sessions: 'users/sessions' }
   get 'posts/index'
   get 'users/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # Defines the root path route ("/")
-  root "users#index"
+ 
   resources :users do
     resources :posts do
       resources :comments
