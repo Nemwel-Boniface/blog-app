d41dc5b23cc8efdcf186c2b1d528ded177aa7d73
unknown <okorojiebube2@gmail.com>
2022-06-21 08:56:13 -0600

    Modify routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 Rails.application.routes.draw do
-  root "users#index"
-  devise_for :users, controllers: { sessions: 'users/sessions' }
-  get 'posts/index'
-  get 'users/index'
+  
+  devise_for :users, :controllers => { registrations: 'users/registrations' }
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # Defines the root path route ("/")
- 
+  root "users#index"
   resources :users do
     resources :posts do
       resources :comments
