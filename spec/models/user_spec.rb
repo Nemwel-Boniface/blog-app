0ecffc1f94d6697a1be6cb71e7fbee86731b8afe
unknown <okorojiebube2@gmail.com>
2022-06-23 16:36:25 -0600

    Modify specs]

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   subject do
-    User.new(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.',
+    User.new(name: 'Faith', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.',
              posts_counter: 2)
   end
 
@@ -13,14 +13,9 @@ RSpec.describe User, type: :model do
     expect(subject).to_not be_valid
   end
 
-  it 'post counter should be greater than 0' do
+  it 'post counter should not be less than 0' do
     subject.posts_counter = -1
     expect(subject).to_not be_valid
-  end
-
-  it 'post counter should be greater than 0' do
-    subject.posts_counter = 1
-    expect(subject).to be_valid
   end
 
   it 'post counter should be an integer' do
