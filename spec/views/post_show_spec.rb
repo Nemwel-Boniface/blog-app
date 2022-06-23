6750f7d671e74a4b9837db8dcef40d1d7bcaea22
unknown <okorojiebube2@gmail.com>
2022-06-23 16:14:09 -0600

    Modify post show test

--- spec/views/post_show_spec.rb
+++ spec/views/post_show_spec.rb
@@ -4,19 +4,24 @@ RSpec.describe 'Posts show page', type: :feature do
     @user = User.create(name: 'Faith', photo: 'profile.png', bio: 'Developer',
                         email: 'faith@email.com', password: 'password',
                         confirmed_at: Time.now, role: 'admin', posts_counter: 0)
+
     @user1 = User.create(name: 'Victor', photo: 'profile.jpg',
                          bio: 'Developer', email: 'victor@email.com',
                          password: 'password', confirmed_at: Time.now)
-    Post.create(title: 'My title', text: 'My text', user_id: @user.id, likes_counter: 0, comments_counter: 0)
-    @comment = Comment.create(text: 'My first comment', user: User.first, post: Post.first)
-    @comment = Comment.create(text: 'My second comment', user: User.first, post: Post.first)
-    @like = Like.create(user_id: User.first.id, post_id: Post.first.id)
+
+    @post = Post.create(user_id: @user.id, title: 'My title', text: 'My text', likes_counter: 0, comments_counter: 0)
+
+    @comment = Comment.create(text: 'My first comment', user: @user, post: @post)
+
+    @comment = Comment.create(text: 'My second comment', user: @user, post: @post)
+
+    @like = Like.create(user_id: @user.id, post_id: @post.id)
 
     visit new_user_session_path
     fill_in 'Email', with: 'victor@email.com'
     fill_in 'Password', with: 'password'
     click_button 'Log in'
-    visit "/users/#{User.first.id}/posts/#{Post.first.id}"
+    visit user_posts_path(@user)
   end
 
   describe 'Specs for view posts#show' do
@@ -25,7 +30,7 @@ RSpec.describe 'Posts show page', type: :feature do
     end
 
     it 'Can see who wrote the post' do
-      expect(page).to have_content 'Lynette'
+      expect(page).to have_content 'Faith'
     end
 
     it 'Can see how many comments it has' do
@@ -41,7 +46,7 @@ RSpec.describe 'Posts show page', type: :feature do
     end
 
     it 'Can see the username of each commentor' do
-      expect(page).to have_content 'Lynette'
+      expect(page).to have_content 'Faith'
     end
 
     it 'Can see the comment each commentor left' do
